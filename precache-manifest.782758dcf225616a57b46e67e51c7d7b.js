2f11588ca741017fe5d2e3cb27af386bc23ad328
AppVeyor <ci@appveyor.com>
2021-02-07 17:20:16 +0000

    07613de

---
+++ precache-manifest.782758dcf225616a57b46e67e51c7d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337b621ec6e2801ac420939899e4aace",
+    "url": "/react-10-2/index.html"
+  },
+  {
+    "revision": "a8ba1b8a67375a810125",
+    "url": "/react-10-2/static/css/main.52874ce9.chunk.css"
+  },
+  {
+    "revision": "cb2f09c2fda7c029d279",
+    "url": "/react-10-2/static/js/2.46daf58d.chunk.js"
+  },
+  {
+    "revision": "a8ba1b8a67375a810125",
+    "url": "/react-10-2/static/js/main.d6df82d0.chunk.js"
+  },
+  {
+    "revision": "116c962dc3fb5a9ea054",
+    "url": "/react-10-2/static/js/runtime-main.a92675c2.js"
+  }
+]);
